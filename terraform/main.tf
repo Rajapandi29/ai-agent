86d0c52b68565f29fc21e9f14c32b852adb89e14
rajapandi29 <rp6321613@gmail.com>
2026-09-21 13:33:06 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -802,7 +802,7 @@ resource "aws_ecs_task_definition" "frontend" {
 
       environment = [
         {
-          name = "NEXT_PUBLIC_API_URL"
+          name  = "NEXT_PUBLIC_API_URL"
           value = "/api"
         }
       ]
